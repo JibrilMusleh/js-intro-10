99d41af57abfac598d6425c0d43a3279ffff503f
JibrilMusleh <jibrilmusleh123@gmail.com>
2024-08-28 15:00:29 -0500

    homework

---
+++ 10-js-objects/06-object-destructuring.js
@@ -0,0 +1,27 @@
+// Array destructuring
+
+const arr = ['abc', 'xyz', 'hi', 'hello'];
+
+//let first = arr[0];
+//let second = arr[1];
+//let third = arr[2];
+//let fourth = arr[3];
+
+const [first, second, third, fouth] = arr;
+
+arr[0] = 'ABC';
+
+console.log(first);
+
+
+let str = 'John';
+
+let name = str;
+
+str = 'James'
+
+console.log(name);
+
+
+//Object destructuring
+
--- homework/homework04.js
+++ homework/homework04.js
@@ -40,11 +40,88 @@ console.log(factorial);
 
 //7
 let attempts = 0;
-let number;
-do {
-    guess = getRandomNumber(1, 100);
-    attempts++;
-} while (guess !== written);
+let ranNum1;
 
-let time = attempts > 1 ? 'times' : 'time';
-console.log(`The random number is ${written} after ${attempts} ${time}!`)
+do {
+    ranNum1 = getRandomNumber(1, 100);
+    attempts++;
+}while(ranNum1 % 5 !== 0);
+
+    console.log(`The random number is ${ranNum1} and it took ${attempts} attempt to generate it`);
+
+//8
+let countries = ['Germany', 'Argentina', 'Ukraine', 'Romania']
+console.log(countries.sort())
+
+//9
+const cartoonDogs = [ 'Scooby Doo', 'Snoopy', 'Blue', 'Pluto', 'Dino', 'Sparky' ];
+
+console.log(cartoonDogs);
+
+for(const Dog of cartoonDogs) {
+    if(Dog.includes('Pluto')) {
+        console.log(false);
+    } 
+    else console.log(true);
+    break;
+}
+
+//10
+const cartoonCats = [ 'Garfield', 'Tom', 'Sylvester', 'Azrael' ];
+
+console.log(cartoonCats.sort());
+
+for (const cat of cartoonCats) {
+    if (cat === 'Garfield' && cat === 'Felix') {
+       console.log(true);
+    }
+    else console.log(false);
+    break;
+}
+
+//11
+const numbers = [10.5, 20.75, 70, 80, 15.75];
+console.log(numbers);
+
+for (const number of numbers) {
+    console.log(number);
+}
+
+//12
+const objects = [ 'Pen', 'notebook', 'Book', 'paper', 'bag', 'pencil', 'Ruler' ];
+let count = 0
+let counter = 0
+
+
+console.log(objects);
+
+for(const object of objects) {
+    if(object.toUpperCase().startsWith('B') || object.toUpperCase().startsWith('P')) count++;
+}
+console.log(count);
+
+for (const object of objects) {
+    if(object.toLowerCase().includes('book') || object.toLowerCase().includes('pen')) counter++
+}
+console.log(counter);
+
+//13
+const numberArr = [ 3, 5, 7, 10, 0, 20, 17, 10, 23, 56, 78 ];
+
+
+let is10 = 0; 
+let moreThan10 = 0; 
+let lessThan10 = 0;
+
+for (let number of numberArr) {
+    if (number < 10) lessThan10++;
+    else if (number > 10) moreThan10++;
+    else is10++;
+}
+
+console.log(numberArr);
+console.log(`Elements that are more than 10 = ${moreThan10}`);
+console.log(`Elements that are less than 10 = ${lessThan10}`);
+console.log(`Elements that are 10 = ${is10}`);
+
+//14
